676c872cefdf45b46aaf3bbbd4cb311cc2cc122d
Egor Denisov <deneg99@mail.ru>
2023-07-15 01:58:32 +0300

    Added deneg-lib

---
+++ firmware/main.c
@@ -0,0 +1,15 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+int main (){
+
+    stdio_init_all();
+
+    while(1){
+        
+    }
+}
